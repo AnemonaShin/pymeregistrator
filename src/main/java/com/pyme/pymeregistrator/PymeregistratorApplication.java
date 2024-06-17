394a297d46c517e9d66f634e098b8b1ba6f86803
Christian Ramirez <cramireza1997@gmail.com>
2024-06-17 16:20:05 -0400

    Primera subida. Inicio de proyecto

---
+++ src/main/java/com/pyme/pymeregistrator/PymeregistratorApplication.java
@@ -0,0 +1,13 @@
+package com.pyme.pymeregistrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymeregistratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymeregistratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyme/pymeregistrator/PymeregistratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyme.pymeregistrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymeregistratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
