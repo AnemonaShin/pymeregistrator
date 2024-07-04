eb435621c5fca542fe4933eb8942d0b3fb9774f0
Christian Ramirez <cramireza1997@gmail.com>
2024-07-04 07:55:58 -0400

    Segunda correccion de ultimo momento dado a falta de ID en url de API para Eliminar dato.

--- src/main/java/com/pyme/pymeregistrator/controller/APIPymeController.java
+++ src/main/java/com/pyme/pymeregistrator/controller/APIPymeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,8 +40,8 @@ public class APIPymeController {
         return service.editInBd(id, body);
     }
     
-    @DeleteMapping("/delete")
-    public ResponseEntity<MessageModel> deletePyme(@RequestParam Long id) throws Exception{
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<MessageModel> deletePyme(@PathVariable Long id) throws Exception{
         return service.deleteInBdById(id);
     }
 }
